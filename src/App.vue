aab640d8ce62c6f7fe79dfc48e735050fe971474
Juan Moreno <juan.moreno1994e@gmail.com>
2025-10-10 20:54:20 -0300

    Agrege la barra de navegacion

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <script setup>
-const nombreapellido = 'Juan Moreno';
+
+import NavBar from './components/icons/NavBar.vue';
+
 </script>
 
 <template>
-  <h1>{{nombreapellido}}</h1>
-  <p>Bienvenido a Vue 3</p>
+  <header>
+     <!-- barra de navegacion -->
+    <NavBar />
+    
+  </header>
+  <main>
+      <section id="educacion"><h2>Educacion - Cursos</h2></section>
+      <section id="experiencia"><h2>Experiencia</h2></section>
+      <section id="proyectos"><h2>Proyectos</h2></section>
+      <section id="habilidades"><h2>Habilidades</h2></section>
+      <section id="intereses"><h2>Intereses</h2></section>
+
+
+  </main>
+
 </template>
 
 <style scoped>
+
 
 </style>

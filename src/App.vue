c4b85450f6648d9a8f23f2160b32da3f0b730d64
Juan Moreno <juan.moreno1994e@gmail.com>
2025-10-24 02:33:13 -0300

    Agrego proyectos y habilidades

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@ import NavBar from './components/icons/NavBar.vue';
 import DatosPersonales from './components/DatosPersonales.vue';
 import Educacion from './components/EducacionComponente.vue';
 import ExperienciaComponente from './components/ExperienciaComponente.vue';
-
+import ProyectosComponente from './components/ProyectosComponente.vue';
+import HabilidadesComponente from './components/HabilidadesComponente.vue';
 </script>
 
 <template>
@@ -18,8 +19,8 @@ import ExperienciaComponente from './components/ExperienciaComponente.vue';
   <main>
       <section id="educacion"><h2>Educacion - Cursos</h2><Educacion /></section>
       <section id="experiencia"><h2>Experiencia</h2><ExperienciaComponente /></section>
-      <section id="proyectos"><h2>Proyectos</h2></section>
-      <section id="habilidades"><h2>Habilidades</h2></section>
+      <section id="proyectos"><h2>Proyectos</h2><ProyectosComponente /></section>
+      <section id="habilidades"><h2>Habilidades</h2><HabilidadesComponente /></section>
       <section id="intereses"><h2>Intereses</h2></section>
 
 
